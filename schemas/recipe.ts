a48d6b34f805785d951a00feb9b105d1d48e5964
Bruno Alla <browniebroke@users.noreply.github.com>
2021-05-25 19:27:50 +0100

    Update recipe document

--- schemas/recipe.ts
+++ schemas/recipe.ts
@@ -23,6 +23,16 @@ export default {
       name: 'title',
       title: 'Title',
       type: 'string',
+      description: 'Name of the recipe.',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      description: 'The path of the recipe page for its URL. Should be unique.',
+      options: {
+        source: 'title',
+      },
     },
     {
       name: 'photo',
@@ -52,6 +62,72 @@ export default {
       of: [
         {
           type: 'text',
+        },
+      ],
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      description:
+        'Quick description of the dish. Displayed at the top of the recipe and in previews.',
+    },
+    {
+      name: 'servings',
+      title: 'Number of people',
+      type: 'number',
+      description: 'Number of portions the dish is for.',
+      validation: (Rule) => Rule.positive().integer(),
+    },
+    {
+      name: 'prepTime',
+      title: 'Preparation time',
+      type: 'number',
+      description: 'In minutes.',
+      validation: (Rule) => Rule.positive().integer(),
+    },
+    {
+      name: 'cookingTime',
+      title: 'Cooking time',
+      type: 'number',
+      description: 'In minutes.',
+      validation: (Rule) => Rule.positive().integer(),
+    },
+    {
+      name: 'isSweet',
+      title: 'Sweet dish?',
+      type: 'boolean',
+      description: 'Check if this is a sweet dish.',
+    },
+    {
+      name: 'isSavoury',
+      title: 'Savoury dish?',
+      type: 'boolean',
+      description: 'Check if this is a savoury dish.',
+    },
+    {
+      name: 'courseType',
+      title: 'Type of course',
+      type: 'string',
+      description: 'Starter, main, dessert....',
+      options: {
+        list: [
+          { title: 'Appetizer', value: 'appetizer' },
+          { title: 'Dessert', value: 'dessert' },
+          { title: 'Main', value: 'main' },
+          { title: 'Snack', value: 'snack' },
+          { title: 'Starter', value: 'starter' },
+        ],
+      },
+    },
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      description: 'Some keywords/tags to classify recipes.',
+      of: [
+        {
+          type: 'string',
         },
       ],
     },
