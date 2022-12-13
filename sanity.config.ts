7fef0fc0cba222e9a7e67c77db5c00ad8fc98ab7
Bruno Alla <alla.brunoo@gmail.com>
2022-12-13 19:01:54 +0000

    feat: add vision tool

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
+import { visionTool } from '@sanity/vision'
 import schemas from './schemas/schema'
 
 export default defineConfig({
   title: 'Cozinha dos Bonitos',
   projectId: 'nufe3788',
   dataset: 'production',
-  plugins: [deskTool()],
+  plugins: [deskTool(), visionTool()],
   schema: {
     types: schemas,
   },
